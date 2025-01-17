aa2d2009ce2a53ad1d4ac91c117258fc16c1b052
niiquash <qua16006@byui.edu>
2025-01-17 10:26:18 -0700

    added characters of the random/current word

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import Header from "./components/Header";
 import Status from "./components/Status";
 import Language from "./components/Language";
 import { languages } from "./languages";
+import Character from "./components/Character";
 
 const App = () => {
   const [chips, setChips] = useState(languages);
+  const [currentWord, setCurrentWord] = useState("REACT");
+
+  const charArray = currentWord.split("");
+  const currentWordChars = charArray.map((ch, idx) => (
+    <Character key={idx} char={ch} />
+  ));
 
   const languageChips = chips.map((chip) => (
     <Language
@@ -21,6 +28,7 @@ const App = () => {
       <Header />
       <Status />
       <div className="languages__container">{languageChips}</div>
+      <div className="characters__container">{currentWordChars}</div>
     </div>
   );
 };
---
+++ src/components/Character.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Character = ({ char }) => {
+  return <span className="character">{char}</span>;
+};
+
+export default Character;
