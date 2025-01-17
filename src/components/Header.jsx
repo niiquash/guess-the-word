2f4b7d525eea93b4f3061996e6aa95de5a7c13a0
niiquash <qua16006@byui.edu>
2025-01-17 01:54:48 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__h1">Guess The Word</h1>
+      <p className="header__introduction">
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
