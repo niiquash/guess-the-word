880b5065c88b0117d2fae241a32c50024f646c65
niiquash <qua16006@byui.edu>
2025-01-17 11:08:34 -0700

    added keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import Status from "./components/Status";
 import Language from "./components/Language";
 import { languages } from "./languages";
 import Character from "./components/Character";
+import Key from "./components/Key";
 
 const App = () => {
   const [chips, setChips] = useState(languages);
   const [currentWord, setCurrentWord] = useState("REACT");
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArray = alphabet.split("");
+
+  const keys = alphabetArray.map((letter, idx) => (
+    <Key key={idx} letter={letter} />
+  ));
 
   const charArray = currentWord.split("");
   const currentWordChars = charArray.map((ch, idx) => (
@@ -29,6 +36,7 @@ const App = () => {
       <Status />
       <div className="languages__container">{languageChips}</div>
       <div className="characters__container">{currentWordChars}</div>
+      <div className="keyboard__container">{keys}</div>
     </div>
   );
 };
---
+++ src/components/Key.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Key = ({ letter }) => {
+  return <span className="key">{letter}</span>;
+};
+
+export default Key;
